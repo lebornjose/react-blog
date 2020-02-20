a154bc5e6cb2149bb91476cb9e6b8d0ff7ffc442
小刀 <chenrongfang@dao.local>
2020-02-20 22:30:13 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import bg from './img/021.webp';
 import './assets/css/all.scss';
+import Home from './components/home';
 
 function App() {
     return (
-        <div className="App">
-            <div className="blog-main">
-                <img src={bg}/>
-            </div>
-        </div>
+        <div>
+            <Router>
+                <div className="home-top container-fluid cover">
+                    <div className="bg">
+                        <img src={bg} alt="背景" className="bg-img"/>
+                    </div>
+                    <div className="home-search">
+                        <div className="searchFrom">
+                            <input type="text" className="input" placeholder="世界这么大，探索一下"/>
+                            <i className="icon icon-search"></i>
+                        </div> 
+                    </div>
+                    <div className="home-menu">
+                
+                        <ul>
+                            <li><Link to="/"><i className="icon-home"></i>&nbsp;主页</Link></li>
+                            <li><a href="#"><i className="icon-book"></i>&nbsp;分类</a></li>
+                            <li><a href="#"><i className="icon-comments"></i>&nbsp;留言</a></li>
+                            <li><a href="#"><i className="icon-book"></i>&nbsp;产品</a></li>
+                            <li><a href="#"><i className="icon-user"></i>&nbsp;关于</a></li>
+                            <li><a href="#"><i className="icon-film"></i>&nbsp;音乐</a></li>
+        
+                        </ul>
+                    </div>
+                </div>
+                <Route path='/' component={Home}></Route>
+            </Router>
+        </div> 
     );
 }
 
