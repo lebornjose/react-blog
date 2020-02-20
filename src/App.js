40e06851953cfe4925fc2e28ec09898990df8471
小刀 <chenrongfang@dao.local>
2020-02-20 10:25:14 +0800

    12

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import bg from './img/021.webp';
+import './assets/css/all.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="blog-main">
+                <img src={bg}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
