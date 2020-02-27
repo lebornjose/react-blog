4b77f3620e5ed9293ee10e24332cc6e39ba2c7e8
小刀 <chenrongfang@dao.local>
2020-02-27 22:07:35 +0800

    评论系统

--- src/components/msg.js
+++ src/components/msg.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import avator from '../img/timg.jpeg';
+import 'gitalk/dist/gitalk.css'
+import GitalkComponent from "gitalk/dist/gitalk-component";
 
-export default () => {
-    return (
-        <div className="container blog-msg">msg</div>
-    )
+class Comment extends React.Component{
+       //方法会在组件已经被渲染到 DOM 中后运行
+    componentDidMount() {
+
+    }
+    render() {
+        return(
+            <div className="container blog-msg">
+            <div className="avator">
+                <img src={avator} />
+            </div>
+            <GitalkComponent options={{
+                clientID: "2f8d419fd8465cb46365",
+                clientSecret: "5eef734ad88addccc0f00d8944d0732bf3aab6e6",
+                repo: 'https://github.com/lebornjose/blog-comment',
+                owner: 'lebornjose',
+                admin: ['lebornjose'],
+                id: "111111",  
+                distractionFreeMode: false 
+                }} />
+        </div>
+        )
+    }
 }
+
+export default Comment;
