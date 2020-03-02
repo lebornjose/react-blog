0608c5b380a92d467e53173b59e42fe75e6bae61
小刀 <chenrongfang@dao.local>
2020-03-02 21:44:20 +0800

    ip地址信息获取

--- src/components/sidebar/component/welcome.js
+++ src/components/sidebar/component/welcome.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import jsonp from '../../../utils/jsonp';
 
-
-export default() => {
-    return(
-      <div className="sidebar-wel sidebar-block">
-          <h3 className="title"><i className="icon-smile"></i>欢迎光临</h3>
-          <div className="content">
-              <p>欢迎来自湖北省的朋友</p>
-              <p>您的 IP 是：171.41.59.35</p>
-              <p>您使用的是：Mac/Chrome79</p>
-          </div>
-      </div>
-    )
+class Welcome extends React.Component{
+    constructor(props) {
+       super(props);
+       this.state = {
+           ip: '',
+           city: ''
+       }
+    }
+    componentWillMount() {
+        // eslint-disable-next-line
+        this.setState({ip: returnCitySN.cip, city: returnCitySN.cname});
+        console.log(this.state.ip)
+    }
+    render() {
+        return (
+            <div className="sidebar-wel sidebar-block">
+                <h3 className="title"><i className="icon-smile"></i>欢迎光临</h3>
+                <div className="content">
+                    <p>欢迎来自{this.state.city}的朋友</p>
+                    <p>您的 IP 是：{this.state.ip}</p>
+                    <p>您使用的是：Mac/Chrome79</p>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default Welcome;
---
+++ src/utils/jsonp.js
@@ -0,0 +1,36 @@
+
+
+export default (url, data) => {
+    return new Promise((resolve, reject) => {
+        // 初始化url
+        let dataString = url.indexOf('?') === -1 ? '?' : '&'
+        let callbackName = `jsonpCB_${Date.now()}`
+        url += `${dataString}callback=${callbackName}`
+        if (data) {
+         // 有请求参数，依次添加到url
+          for (let k in data) {
+            url += `&${k}=${data[k]}`
+          }
+        }
+        let jsNode = document.createElement('script')
+        jsNode.src = url
+        // 触发callback，触发后删除js标签和绑定在window上的callback
+        window[callbackName] = result => {
+          delete window[callbackName]
+          document.body.removeChild(jsNode)
+          if (result) {
+            resolve(result)
+          } else {
+            reject('没有返回数据')
+          }
+        }
+        // js加载异常的情况
+        jsNode.addEventListener('error', () => {
+          delete window[callbackName]
+          document.body.removeChild(jsNode)
+          reject('JavaScript资源加载失败')
+        }, false)
+        // 添加js节点到document上时，开始请求
+        document.body.appendChild(jsNode)
+    })
+};
